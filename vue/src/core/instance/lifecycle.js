b2b91afbcadfc5940f2ed6c7045fceafee6a65e2
WangChiech <wangchiech@yeah.net>
2020-09-17 20:48:30 +0800

    vue 数据驱动

--- vue/src/core/instance/lifecycle.js
+++ vue/src/core/instance/lifecycle.js
@@ -187,6 +187,7 @@ export function mountComponent (
   // we set this to vm._watcher inside the watcher's constructor
   // since the watcher's initial patch may call $forceUpdate (e.g. inside child
   // component's mounted hook), which relies on vm._watcher being already defined
+  // 渲染watch，和响应原理强相关的一个类
   new Watcher(vm, updateComponent, noop, {
     before () {
       if (vm._isMounted) {
--- vue/src/platforms/web/entry-runtime-with-compiler.js
+++ vue/src/platforms/web/entry-runtime-with-compiler.js
@@ -19,21 +19,23 @@ Vue.prototype.$mount = function (
   el?: string | Element,
   hydrating?: boolean
 ): Component {
+  // 如果传入了el，通过query方法将el转换成DOM
   el = el && query(el)
 
   /* istanbul ignore if */
+  // 不能直接挂在到body或html上（覆盖后页面结构不对），开发环境发出警告
   if (el === document.body || el === document.documentElement) {
     process.env.NODE_ENV !== 'production' && warn(
       `Do not mount Vue to <html> or <body> - mount to normal elements instead.`
     )
     return this
   }
-
+  // 挂载$options
   const options = this.$options
   // resolve template/el and convert to render function
-  if (!options.render) {
+  if (!options.render) { // 没有手写render
     let template = options.template
-    if (template) {
+    if (template) { // 如果有template
       if (typeof template === 'string') {
         if (template.charAt(0) === '#') {
           template = idToTemplate(template)
@@ -54,6 +56,7 @@ Vue.prototype.$mount = function (
         return this
       }
     } else if (el) {
+      // template为字符串
       template = getOuterHTML(el)
     }
     if (template) {
@@ -86,6 +89,7 @@ Vue.prototype.$mount = function (
  * of SVG elements in IE as well.
  */
 function getOuterHTML (el: Element): string {
+  // DOM.outerHtml将DOM HTML转换成字符串
   if (el.outerHTML) {
     return el.outerHTML
   } else {
